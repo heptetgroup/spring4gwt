4c690182d5c165cbe23ef16990f07f6335bb0425
Kay McCormick <kay.elearning@gmail.com>
2014-03-23 11:06:22 -0700

    changed logging dependency and had to disable compilation of examples

--- src/main/java/org/spring4gwt/server/SpringGwtRemoteServiceServlet.java
+++ src/main/java/org/spring4gwt/server/SpringGwtRemoteServiceServlet.java
@@ -2,7 +2,8 @@ package org.spring4gwt.server;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
@@ -16,7 +17,7 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 @SuppressWarnings("serial")
 public class SpringGwtRemoteServiceServlet extends RemoteServiceServlet {
 
-	private static final Logger LOG = Logger.getLogger(SpringGwtRemoteServiceServlet.class);
+	private static final Logger LOG = LoggerFactory.getLogger(SpringGwtRemoteServiceServlet.class);
 
 	@Override
 	public void init() {
